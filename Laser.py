8e962abeb7a0ac7b4a2688be10d0d49dfcbb1d35
James Horwitz <jhorwitz1011@gmail.com>
2020-09-23 16:43:52 -0400

    finished off_screen & removed unnecessary variable in move

--- Laser.py
+++ Laser.py
@@ -18,11 +18,14 @@ class Laser:
         window.blit(self.img, (self.x, self.y))
 
     ### Laser moves in a straight line. Optional: add logic for bouncing off of edge
-    ### PASS THRU WINDOW MAX X AND Y PLS!!!
-    def move(self, max_x, max_y):
+    ### PASS THRU WINDOW MAX X PLS!!!
+    def move(self, max_x):
         if (self.x > max_x):
             self.rotation = -self.rotation
-        self.x += self.speed*math.sin(rotation)
-        self.y += self.speed*math.cos(rotation)
+        self.x += self.speed*math.sin(self.rotation)
+        self.y += self.speed*math.cos(self.rotation)
     ##?!?!?!?! wtf
-    def off_screen(self):
+    def off_screen(self, max_y):
+        if self.y > max_y:
+            return True
+        return False
